db29ceaacb43254ebc4ec8e9a4baba736377dd35
rezkarimian <mrezakya@gmail.com>
2026-07-24 14:59:56 +0330

    Add files via upload

---
+++ Students/MohammadRezaKarimian_402412372/Simulation/tb_cpu.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+
+module tb_cpu;
+
+	reg Clock;
+	reg Reset;
+
+	CPU UUU (Clock,Reset);
+	
+
+	initial begin
+
+		Clock = 0;
+		Reset = 1;
+
+		#5;
+		
+		Reset=0;
+
+	end
+      always #10 Clock=~Clock;
+endmodule
+
